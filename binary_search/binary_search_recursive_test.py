a2a10af2367f946ddc981b362c3fffd4aa0b30f5
Dileep Rajput <dileep_rajput@yahoo.com>
2019-06-19 08:15:38 -0400

    Add tests for recursive binary search

---
+++ binary_search/binary_search_recursive_test.py
@@ -0,0 +1,31 @@
+import unittest
+from binary_search_recursive import binary_search_recursive
+
+class TestBinarySearchRecursive(unittest.TestCase):
+
+    def test_search_for_lowest_val(self):
+        arr = [1,2,3,4,5]
+        val = 1
+        result = binary_search_recursive(arr, val, 0, len(arr))
+        self.assertEqual(result, 0, "Should be 0")
+
+    def test_search_for_highest_val(self):
+        arr = [1,2,3,4,5]
+        val = 5
+        result = binary_search_recursive(arr, val, 0, len(arr))
+        self.assertEqual(result, 4, "Should be 4")
+
+    def test_search_for_immediately_finding_val(self):
+        arr = [1,2,3,4,5]
+        val = 3
+        result = binary_search_recursive(arr, val, 0, len(arr))
+        self.assertEqual(result, 2, "Should be 2")
+
+    def test_search_for_unfindable_val(self):
+        arr = [1,2,3,4,5]
+        val = 1000
+        result = binary_search_recursive(arr, val, 0, len(arr))
+        self.assertEqual(result, -1, "Should be -1")
+
+if __name__ == '__main__':
+    unittest.main()

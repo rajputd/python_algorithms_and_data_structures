6785505caedddde79e3d002822b5ed98a5e36082
Dileep Rajput <dileep_rajput@yahoo.com>
2019-06-25 07:58:32 -0400

    Add insertion sort and tests

---
+++ insertion_sort/InsertionSort.py
@@ -0,0 +1,23 @@
+def insertionSort(arr):
+    output = arr[:]
+
+    #go through each element
+    for index in range(1, len(output)):
+        #grab current element
+        current = output[index]
+
+        #get last index of sorted outputay
+        j = index
+
+        #shift up all sorted elements that are greater than current one
+        while j > 0 and output[j - 1] > current:
+            output[j] = output[j - 1]
+            j = j - 1
+
+        #insert current element at newly created space
+        output[j] = current
+
+    return output
+
+
+

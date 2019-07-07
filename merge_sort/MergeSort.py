abb40d753f51b5d784e22cb5ee0ec75d56454fb8
Dileep Rajput <dileep_rajput@yahoo.com>
2019-07-07 18:15:05 -0400

    Add comments to MergeSort.py

--- merge_sort/MergeSort.py
+++ merge_sort/MergeSort.py
@@ -16,13 +16,18 @@ def mergeSort(arr):
         left_index = 0
         right_index = 0
         for i in range(len(output)):
+            #if there are unsorted elements remaining in the right and left halves
             if left_index < len(left) and right_index < len(right):
+                #if the left array has the smaller value
                 if left[left_index] <= right[right_index]:
+                    #add that value into the output array and continue
                     output[i] = left[left_index]
                     left_index += 1
                 else:
+                    #otherwise move the value in the right half to the output array
                     output[i] = right[right_index]
                     right_index += 1
+            #else if we have inserted all the values in one array then insert the rest from the other
             elif left_index < len(left):
                 output[i] = left[left_index]
                 left_index += 1
